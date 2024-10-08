1c92d8a08b1a3a5de7423705df42a0a3ccd4b022
David Will <pfefferundsalz@posteo.de>
2024-10-08 18:59:06 +0200

    receiving messages and passing the update along

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,3 +1,17 @@
-export default async function handler(req, res) {
+import 'dotenv/config';
+import { Telegraf } from 'telegraf';
+import { message } from 'telegraf/filters'
+
+const bot = new Telegraf(process.env.TELEGRAM_PILLBOT_TOKEN);
+bot.on(message('text'), ctx => {
+    console.log('Received text message:', ctx.message.text);
+})
+
+export default async function handler(request, context) {
     console.log('Request received');
+
+    const body = await request.json();
+    bot.handleUpdate(body);
+
+    return new Response('OK', { status: 200 });
 }
