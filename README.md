bc00dc8743ce9b397b901ad05809f38411627788
David Will <pfefferundsalz@posteo.de>
2024-10-08 19:51:18 +0200

    added auth check

--- README.md
+++ README.md
@@ -3,11 +3,12 @@
 ## TODO
 - [x] init `package.json`
 - [x] init git
-- [x] set up telegram bot
-- [x] drop credentials in `.env`
+- [x] set up telegram bot & drop credentials in `.env`
 - [x] init netlify
-- [ ] create netlify live server
-- [ ]  
+- [x] create netlify live server & drop dev url in `.env`
+- [x] set webhook and verify at https://api.telegram.org/bot<token>/getWebhookInfo
+- [x] pass update to bot 
+- [x] add auth check
 
 
 
--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -9,8 +9,20 @@ bot.on(message('text'), ctx => {
 
 export default async function handler(request, context) {
     console.log('Request received');
-
+    
     const body = await request.json();
+    const userID = body?.message?.from?.id;
+
+    if (!userID) {
+        console.error('Invalid request body:', body);
+        return new Response('Invalid request body', { status: 400 });
+    }
+
+    if (userID !== Number(process.env.TELEGRAM_USER_ID)) {
+        console.error('Unauthorized request:', userID);
+        return new Response('Unauthorized', { status: 401 });
+    }
+
     bot.handleUpdate(body);
 
     return new Response('OK', { status: 200 });
